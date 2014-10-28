32427607b52be383affcbe2751b616787243d03b
Terry Chun Wong <terrywong283@live.com>
2014-10-28 00:50:31 -0700

    Added 'Return' button to Upload/Export page

--- observations/templatetags/observations.py
+++ observations/templatetags/observations.py
@@ -16,3 +16,12 @@ def list_get_app_name(value):
     """Get the app name in the list view"""
     url_parts = value.rsplit('/', 2)
     return url_parts[1]
+    
+@register.filter
+@stringfilter
+def trim_url(value):
+    """Trim url for the correct path"""
+    url_parts = value.split('/', 4)
+    url_parts.remove(url_parts[4])
+    url = '/'.join(url_parts)
+    return url
