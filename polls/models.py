63f6be9f74a7a3495df88460ea5e0d2edc4864c5
Terry Chun Wong <terrywong283@live.com>
2015-01-03 22:49:10 -0800

    Updated requirements.txt

--- polls/models.py
+++ polls/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.question
     def was_published_recently(self):
@@ -18,5 +19,6 @@ class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.choice_text
